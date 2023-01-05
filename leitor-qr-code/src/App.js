9863534f6368578bcbbfe26f9c8c9b66d2cedeed
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2023-01-05 16:01:35 -0300

    colocando estilo css e fazendo a logica do react

--- leitor-qr-code/src/App.js
+++ leitor-qr-code/src/App.js
@@ -1,9 +1,39 @@
-import './App.css';
+import { useState } from "react";
+import QRCode from "react-qr-code";
+import QRCodeLink from "qrcode";
+
+import "./App.css";
 
 function App() {
+  const [link, setLink] = useState("");
+  const [qrCodeLink, setQrCodeLink] = useState("")
+  
+  function handleGenerate(link_url) {
+    QRCodeLink.toDataURL(link_url,{
+      width: 600,
+      margin: 3,
+    }, function(err, url){
+      setQrCodeLink(url);
+    });
+  }
+  function handleChange(e) {
+    setLink(e.target.value);
+    handleGenerate(e.target.value);
+  }
+
+
   return (
-    <div>
-      <>QR Code</>
+    <div className="container">
+      <QRCode value={link} />
+
+      <input
+        className="input"
+        placeholder="Digite seu link..."
+        value={link}
+        onChange={(e) => handleChange(e)}
+      />
+    <a href={qrCodeLink} download={`qrcode.png`}>Baixar QR Code</a>
+    
     </div>
   );
 }
